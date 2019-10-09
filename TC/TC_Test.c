65eb929c4de0d3facbe1ee1d1b359971f3ac09ce
Aeather <aeather@naver.com>
2019-10-09 20:28:18 +0900

    Program Spend Time Added

--- TC/TC_Test.c
+++ TC/TC_Test.c
@@ -1,3 +1,4 @@
+#include <time.h>
 #include "TC_Calculator.h"
 #include "stdfx.h"
 
@@ -5,9 +6,12 @@ int main(void)
 {
     char Infix[] = "1 + 3.334 / (4.28*(110-7729))";
 
+    clock_t t = clock();
     double Result = TreeCalculator(Infix);
+    t = t - clock();
 
-    printf("Infix : %s\nResult : %lf", Infix, Result);
+    printf("Infix : %s\nResult : %lf\n", Infix, Result);
+    printf("Spent Time : %lf\n", (double)t / CLOCKS_PER_SEC);
 
     return 0;
 }
